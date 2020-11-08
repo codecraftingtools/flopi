3288185f4594c52a09cbe949892803efc572a6a5
Jeff Webb <jeff.webb@codecraftsmen.org>
2020-11-08 06:39:17 -0600

    Incremented version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="flopi",
-    version="0.0.1",
+    version="0.0.2",
     author="Jeff Webb",
     author_email="jeff.webb@codecraftsmen.org",
     description="Fine-grained logging and output Python interface",
